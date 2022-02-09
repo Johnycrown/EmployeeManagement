54aba08a5d372e91a2828f178749570fd6973940
Johnycrown <oladejijohn60@gmail.com>
2022-02-09 11:01:25 +0100

    delete employee updated in the service layer

--- src/main/java/com/EmployeeManagementApplication/demo/service/EmployeeService.java
+++ src/main/java/com/EmployeeManagementApplication/demo/service/EmployeeService.java
@@ -2,14 +2,18 @@ package com.EmployeeManagementApplication.demo.service;
 
 import com.EmployeeManagementApplication.demo.data.dto.EmployeeDto;
 import com.EmployeeManagementApplication.demo.data.mdoel.Employee;
+import com.EmployeeManagementApplication.demo.web.exceptions.EmployeeException;
+import com.github.fge.jsonpatch.JsonPatch;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeService {
     Employee addEmplyee(EmployeeDto employeeDto);
     List<Employee> findAllEmployee();
-    Employee  updateEmploye(Long employeeId);
-    void deleteEmployee();
+    Employee  updateEmploye(Long employeeId, JsonPatch employeePatch) throws EmployeeException;
+    void deleteEmployee(Long employeeId);
     Employee getEmployeeById(Long employeeId);
 
 }
