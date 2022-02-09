7eaba2796caf003cd98e1d885292f6c3c37d341b
Johnycrown <oladejijohn60@gmail.com>
2022-02-09 06:01:54 +0100

    implementation

---
+++ src/main/java/com/EmployeeManagementApplication/demo/data/dto/EmployeeDto.java
@@ -0,0 +1,8 @@
+package com.EmployeeManagementApplication.demo.data.dto;
+
+public class EmployeeDto {
+    private String firstName;
+    private String lastName;
+    private String Address;
+    private String phoneNumber;
+}
--- src/main/java/com/EmployeeManagementApplication/demo/service/EmployeeService.java
+++ src/main/java/com/EmployeeManagementApplication/demo/service/EmployeeService.java
@@ -1,4 +1,15 @@
 package com.EmployeeManagementApplication.demo.service;
 
+import com.EmployeeManagementApplication.demo.data.dto.EmployeeDto;
+import com.EmployeeManagementApplication.demo.data.mdoel.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+    Employee addEmplyee(EmployeeDto employeeDto);
+    List<Employee> findAllEmployee();
+    Employee  updateEmploye(Long employeeId);
+    void deleteEmployee();
+    Employee getEmployeeById(Long employeeId);
+
 }
